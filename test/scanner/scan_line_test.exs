13956fe8bc502ceb0d44dc03e04474cf2686ea04
RobertDober <robert.dober@gmail.com>
2015-12-31 10:53:19 +0100

    UnderlineHeadlines

--- test/scanner/scan_line_test.exs
+++ test/scanner/scan_line_test.exs
@@ -4,49 +4,58 @@ defmodule Earmark.Scanner.ScanLineTest do
   alias Earmark.Scanner
 
   [ 
-    { "", [%Scanner.EmptyLine{}] },
-    { "alpha beta", [%Scanner.Text{content: "alpha beta"}] },
-    { " alpha", [%Scanner.Text{content: " alpha"}] },
-    { "   alpha", [%Scanner.Text{content: "   alpha"}] },
+    { "",           [ %Scanner.EmptyLine{}] },
+    { "alpha beta", [ %Scanner.Text{content: "alpha beta"}] },
+    { " alpha",     [ %Scanner.Text{content: " alpha"}] },
+    { "   alpha",   [ %Scanner.Text{content: "   alpha"}] },
 
     # Leading Whitespace
-    { "    alpha", [%Scanner.LeadingWS{count: 4},%Scanner.Text{content: "alpha"}] },
-    { "     alpha", [%Scanner.LeadingWS{count: 5},%Scanner.Text{content: "alpha"}] },
-    { "     # no headline", [%Scanner.LeadingWS{count: 5},%Scanner.Text{content: "# no headline"}] },
+    { "    alpha",          [ %Scanner.LeadingWS{count: 4},%Scanner.Text{content: "alpha"}] },
+    { "     alpha",         [ %Scanner.LeadingWS{count: 5},%Scanner.Text{content: "alpha"}] },
+    { "     # no headline", [ %Scanner.LeadingWS{count: 5},%Scanner.Text{content: "# no headline"}] },
 
     # Rulers
-    { "***", [%Scanner.Ruler{content: "***", type: "*"}] },
-    { "_ ___ _", [%Scanner.Ruler{content: "_ ___ _", type: "_"}] },
-    { "---", [%Scanner.Ruler{content: "---", type: "-"}] },
-    { "_ _", [%Scanner.Text{content: "_ _"}] },
-    { "**", [%Scanner.Text{content: "**"}] },
+    { "***",     [ %Scanner.RulerFat{}] },
+    { "_ ___ _", [ %Scanner.RulerMedium{}] },
+    { "---",     [ %Scanner.RulerThin{}] },
+    { "  ---",   [ %Scanner.RulerThin{}] },
+    { "_ _",     [ %Scanner.Text{content: "_ _"}] },
+    { "**",      [ %Scanner.Text{content: "**"}] },
 
     # Headlines
-    { "#", [%Scanner.Text{content: "#"}]},
-    { " #", [%Scanner.Text{content: " #"}]},
-    { "##", [%Scanner.Text{content: "##"}]},
-    { "###xxx", [%Scanner.Text{content: "###xxx"}]},
-    { "# ", [%Scanner.Headline{level: 1}]},
-    { "###  ", [%Scanner.Headline{level: 3}]},
+    { "#",      [ %Scanner.Text{content: "#"}]},
+    { " #",     [ %Scanner.Text{content: " #"}]},
+    { "##",     [ %Scanner.Text{content: "##"}]},
+    { "###xxx", [ %Scanner.Text{content: "###xxx"}]},
+    { "# ",     [ %Scanner.Headline{level: 1}]},
+    { "###  ",  [ %Scanner.Headline{level: 3}]},
     { "###### Hello `World`", [
       %Scanner.Headline{level: 6},
       %Scanner.Text{content: "Hello "},
       %Scanner.Backtix{count: 1},
       %Scanner.Text{content: "World"},
       %Scanner.Backtix{count: 1}]},
-    { "####   3 * 2", [%Scanner.Headline{level: 4},%Scanner.Text{content: "3 * 2"}]},
-    { "####### text", [%Scanner.Text{content: "####### text"}]},
-    { " # Hello", [%Scanner.Text{content: " # Hello"}]},
+    { "####   3 * 2", [ %Scanner.Headline{level: 4},%Scanner.Text{content: "3 * 2"}]},
+    { "####### text", [ %Scanner.Text{content: "####### text"}]},
+    { " # Hello",     [ %Scanner.Text{content: " # Hello"}]},
 
     # Backtix
-    { "`", [%Scanner.Backtix{count: 1}]},
-    { " `````a", [%Scanner.Text{content: " "},%Scanner.Backtix{count: 5},%Scanner.Text{content: "a"}]},
-    { "     `````a", [%Scanner.LeadingWS{count: 5},%Scanner.Backtix{count: 5},%Scanner.Text{content: "a"}]},
+    { "`",           [ %Scanner.Backtix{count: 1}]},
+    { " `````a",     [ %Scanner.Text{content: " "},%Scanner.Backtix{count: 5},%Scanner.Text{content: "a"}]},
+    { "     `````a", [ %Scanner.LeadingWS{count: 5},%Scanner.Backtix{count: 5},%Scanner.Text{content: "a"}]},
 
     # CodeFences
-    { "~~~", [%Scanner.CodeFence{}]},
-    { "  ~~~alpha", [%Scanner.Text{content: "  "},%Scanner.CodeFence{}, %Scanner.Text{content: "alpha"}]},
+    { "~~~",        [ %Scanner.CodeFence{}]},
+    { "  ~~~alpha", [ %Scanner.Text{content: "  "},%Scanner.CodeFence{}, %Scanner.Text{content: "alpha"}]},
 
+    # UnderHeadline N.B. Semantics of RulerThin depend on the context
+    { "= ",      [ %Scanner.UnderHeadline{level: 1}]},
+    { "-",       [ %Scanner.UnderHeadline{level: 2}]},
+    { "-- ",     [ %Scanner.UnderHeadline{level: 2}]},
+    { "--- ",    [ %Scanner.RulerThin{}]},
+    { " =",      [ %Scanner.Text{content: " ="}]},
+    { "- =",     [ %Scanner.Text{content: "- ="}]},
+    { "    ---", [ %Scanner.LeadingWS{count: 4}, %Scanner.Text{content: "---"}]},
   ]
   |> Enum.each(fn { text, tokens } -> 
     test("line: '" <> text <> "'") do
